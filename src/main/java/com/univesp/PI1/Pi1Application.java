91adb7e48a815639f09ed7db7b72ac7465befca5
Julio César Gama <juliuscgama@outlook.com>
2023-03-31 01:33:22 -0300

    Initial commit

---
+++ src/main/java/com/univesp/PI1/Pi1Application.java
@@ -0,0 +1,13 @@
+package com.univesp.PI1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/univesp/PI1/Pi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univesp.PI1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
